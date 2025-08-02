e3d6a7a67e31cb7fa4ea4ded87a047275be9289b
iankressin <ianguimaraes31@gmail.com>
2025-08-02 09:39:04 -0300

    chore: clean up main module

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
 import { program } from "./program";
-import { Effect, succeed, fail } from "effect/Effect"
-
-const value = succeed(10)
-const error = fail(new Error('An error'))
-
-function divide(a: number, b: number): Effect<number, Error> {
-    return b === 0 ? succeed(a / b) : fail(new Error('cannot devide by 0'))
-}
 
 function main() {
     program.parse(process.argv)
