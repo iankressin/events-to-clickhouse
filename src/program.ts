2a49331c932977dcd8e4cdd048b75ce68d50800f
iankressin <ianguimaraes31@gmail.com>
2025-08-29 18:57:29 -0300

    chore: bump version

--- src/program.ts
+++ src/program.ts
@@ -7,7 +7,7 @@ export const program = new Command()
 program
   .name('events-to-tables')
   .description('Create DB tables from EVM events')
-  .version('0.0.6')
+  .version('0.0.7')
 
 program
   .command('generate')
